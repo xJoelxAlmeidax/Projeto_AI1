eeb4f4eeca63a4f021cff58522d929822c6315c7
xJoelxAlmeidax <98495737+xJoelxAlmeidax@users.noreply.github.com>
2024-11-20 20:38:07 +0000

    Add files via upload

---
+++ SCRIPT/geral.js
@@ -0,0 +1,53 @@
+// Seleciona o link do logo e os elementos h1 e h5
+const logo = document.querySelector(".home");
+const title = document.querySelector(".home h1");
+const subtitle = document.querySelector(".home h5");
+
+// Adiciona evento para quando o rato entra no logo
+logo.addEventListener("mouseenter", () => {
+    title.classList.remove("hidden");
+    subtitle.classList.remove("hidden");
+});
+
+// Adiciona evento para quando o rato sai do logo
+logo.addEventListener("mouseleave", () => {
+    title.classList.add("hidden");
+    subtitle.classList.add("hidden");
+});
+
+
+//DATA E HORA
+
+function data_horaF(){
+    const dateTimeElement = document.getElementById('data_hora');
+    const now = new Date();
+
+    // Formatar a data e hora
+    const options = { 
+        year: 'numeric', 
+        month: 'numeric', 
+        day: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit', 
+    };
+    const formattedDateTime = now.toLocaleDateString('pt-PT', options);
+
+    // Atualizar o elemento com a data e hora formatada
+    dateTimeElement.textContent = formattedDateTime;
+}
+
+setInterval(data_horaF, 1000);
+
+data_horaF();
+
+function abrirSidebar() {
+    document.getElementById("mySidebar").classList.add("open");
+    document.getElementById("overlay").style.display = "block";
+    document.body.style.overflow = "hidden"; 
+  }
+
+  function fecharSidebar() {
+    document.getElementById("mySidebar").classList.remove("open");
+    document.getElementById("overlay").style.display = "none";
+    document.body.style.overflow = ""; 
+  }
---
+++ SCRIPT/script_index.js
@@ -0,0 +1,81 @@
+//SLIDER
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+const slidesContainer = document.querySelector(".slide");
+// Intervalo automático do carrossel
+setInterval(function () {
+  nextImage();
+}, 4000);
+
+function nextImage() {
+  if (count == 5) {
+    // Exibe o slide 6 por apenas 1 segundo
+    document.getElementById("radio6").checked = true;
+    document.getElementById("Artista").innerHTML = "Artista Destacado: Taylor Swift";
+    setTimeout(() => {count = 1;
+      slidesContainer.style.transition = "none"; // Remove transição temporariamente
+      document.getElementById("radio1" ).checked = true;
+
+      // Restaura a transição para os próximos slides
+      setTimeout(() => {
+        slidesContainer.style.transition = ""; // Retorna à transição padrão
+      }, 50); // Tempo curto para o slide 6 (1 segundo)
+    }, 1000);
+
+  } 
+  else {
+    // Lógica normal para os outros slides
+    count++;
+    document.getElementById("radio" + count).checked = true;
+
+    switch (count) {
+      case 1:
+        document.getElementById("Artista").innerHTML = "Artista Destacado: Taylor Swift";
+        break;
+      case 2:
+        document.getElementById("Artista").innerHTML = "Artista Destacado: Noah Kahan";
+        break;
+      case 3:
+        document.getElementById("Artista").innerHTML = "Artista Destacado: Phoebe Bridgers";
+        break;
+      case 4:
+        document.getElementById("Artista").innerHTML = "Artista Destacado: Finneas O'Connel";
+        break;
+      case 5:
+        document.getElementById("Artista").innerHTML = "Artista Destacado: Gracie Abrams";
+        break;
+    }
+  }
+}
+
+
+// Função para selecionar manualmente um slide
+function setManualSelection(index) {
+  count = index;
+  document.getElementById("radio" + count).checked = true;
+  switch (count) {
+    case 1:
+      document.getElementById("Artista").innerHTML = "Artista Destacado: Taylor Swift";
+      break;
+    case 2:
+      document.getElementById("Artista").innerHTML = "Artista Destacado: Noah Kahan";
+      break;
+    case 3:
+      document.getElementById("Artista").innerHTML = "Artista Destacado: Phoebe Bridgers";
+      break;
+    case 4:
+      document.getElementById("Artista").innerHTML = "Artista Destacado: Finneas O'Connel";
+      break;
+    case 5:
+      document.getElementById("Artista").innerHTML = "Artista Destacado: Gracie Abrams";
+      break;
+  }
+}
+
+// Adiciona evento aos botões de rádio
+for (let i = 1; i <= 6; i++) {
+  document.getElementById("radio" + i).addEventListener("click", function () {
+    setManualSelection(i);
+  });
+}
